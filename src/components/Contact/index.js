8cc9811aab7e4813b5c655c3ddbf6b693206ead8
Robert Martin <rlmartin1996@gmail.com>
2023-01-12 10:14:09 -0600

    whatsapp

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -40,7 +40,7 @@ const Contact = () => {
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>Coming Soon</h5>
-            <a href="https://api.whatsapp.com/send?phone=+1234567890" target="_blank">Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=+3055868438" target="_blank">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
