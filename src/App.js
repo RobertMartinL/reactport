c83535e9424bb5c0d8023b08fbed54c014bdc272
Robert Martin <rlmartin1996@gmail.com>
2023-01-13 16:12:51 -0600

    remove unused components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 import About from './components/About'
 import Experience from './components/Experience'
-import Services from './components/Services'
 import Portfolio from './components/Portfolio'
-// import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
@@ -16,9 +14,7 @@ const App = () => {
         <Nav />
         <About />
         <Experience />
-        {/* <Services /> */}
         <Portfolio />
-        {/* <Testimonials /> */}
         <Contact />
         <Footer />
     </>
